97ff3101952bb5069678acf263c3ac3216c7ea35
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2023-12-19 12:25:59 +0000

    create handler for set data

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,12 @@ var containerEl = $(".container");
 // Display the current day at the top of the calendar when a user opens the planner.
 const displayTime = () => dayjs().format("DD MMM YYYY");
 currentDayEl.text(displayTime());
+
+// Handler for set data
+let setData = (item) => {
+  let data = getData(); // call getdata handler for getting  data from list
+  data = data != false ? data : [];
+  data.push(item);
+  data = JSON.stringify(data);
+  localStorage.setItem("dailySchedule", data);
+};
