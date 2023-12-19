4a19cee98b8e023c75e3c80a8063bf5602f6cb7f
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2023-12-19 12:25:30 +0000

    display the current day at the top

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
 // save reference to important DOM elements
 var currentDayEl = $("#currentDay");
 var containerEl = $(".container");
+
+// Display the current day at the top of the calendar when a user opens the planner.
+const displayTime = () => dayjs().format("DD MMM YYYY");
+currentDayEl.text(displayTime());
